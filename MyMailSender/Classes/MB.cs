5d2d30188c5cfe1719b3ad5f80c51e496c49a129
Denis Anisimov <den.anisimov.94@mail.ru>
2020-01-30 20:24:15 +0300

    Написал код для расслыки

---
+++ MyMailSender/Classes/MB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace MyMailSender.Classes
+{
+    static class MB
+    {
+        public static void MessageBoxError(string information)
+        {
+            MessageBox.Show(information, "Ошибка",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public static void MessageBoxInfo(string information)
+        {
+            MessageBox.Show(information, "Уведмоление",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
--- MyMailSender/Window/MainWindow.xaml.cs
+++ MyMailSender/Window/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using MyMailSender.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,7 +28,65 @@ namespace MyMailSender
         {
             InitializeComponent();
 
-            menuItemExit.Click += delegate { this.Close(); };
+            MenuItemExit.Click += delegate { Application.Current.Shutdown(); };
+        }
+
+        private void btnSendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextBoxEmail.Text))
+            {
+                MB.MessageBoxError("Введите email.");
+                TextBoxEmail.Focus();
+            }
+            else if (string.IsNullOrEmpty(TextBoxPassword.Password))
+            {
+                MB.MessageBoxError("Введите пароль.");
+                TextBoxPassword.Focus();
+            }
+            else if (string.IsNullOrEmpty(TextBoxTo.Text))
+            {
+                MB.MessageBoxError("Введите email, кому вы отправляете сообщение.");
+                TextBoxEmail.Focus();
+            }
+            else if (string.IsNullOrEmpty(TextBoxMessage.Text))
+            {
+                MB.MessageBoxError("Введите сообщение.");
+                TextBoxMessage.Focus();
+            }
+            else
+            {
+                try
+                {
+                    using (var message = new MailMessage(TextBoxEmail.Text, TextBoxTo.Text))
+                    {
+                        message.Subject = TextBoxSubject.Text;
+                        message.Body = TextBoxMessage.Text;
+
+
+                        const string serverAddress = "smtp.mail.ru";
+                        const int serverPort = 25; 
+                        using (var client = new SmtpClient(serverAddress, serverPort))
+                        {
+                            client.EnableSsl = true;
+
+                            var userName = TextBoxEmail.Text;
+                            //var user_password = PasswordEdit.Password;
+                            SecureString userPassword = TextBoxPassword.SecurePassword;
+
+                            client.Credentials = new NetworkCredential(userName, userPassword);
+
+                            client.Send(message);
+
+                            MessageBox.Show("Почта отправлена!", "Ура!!!",
+                                MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                    }
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
